eab71736055d4c2cfc39fe24a9e0dfd2a0cbd8a2
Valentin <contact@eywek.fr>
2017-01-22 15:18:20 +0100

    fix

--- app/authenticate.php
+++ app/authenticate.php
@@ -288,7 +288,7 @@ if($request['method'] == "POST") {
 
 			// log into log database
 	        Core\Queries::execute(
-				"INSERT INTO loginlogs(username, ip, type) VALUES(:username, :ip, :mac_adress, :type)",
+				"INSERT INTO loginlogs(username, ip, mac_adress, type) VALUES(:username, :ip, :mac_adress, :type)",
 				[
 					'username' => $username,
 					'ip'	  => $_SERVER['REMOTE_ADDR'],
